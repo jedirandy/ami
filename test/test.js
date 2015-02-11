d855809b7d073fac61eca651b32c06d753701dff
Sheng <jedirandy@gmail.com>
2015-02-11 00:52:44 -0500

    init commit

---
+++ test/test.js
@@ -0,0 +1,41 @@
+var ami = require('../ami');
+var assert = require("assert");
+
+describe('test ami', function(){
+	it('should have 2 properties', function(done){
+		ami([
+				function(context, next) {
+					context.name = 'hello';
+					(function(){
+						next();
+					})();
+				},
+				function(context, next) {
+					context.name2 = 'apc';
+					next();
+				}
+			], function(context){
+			assert.equal(context.name, 'hello');
+			assert.equal(context.name2, 'apc');
+			done();
+		});
+	});
+
+	it('jump to main function when no module is present', function(){
+		ami([], function(context){
+			assert.deepEqual(context, {});
+		});
+	});
+
+	it('passing invalid modules throws an error',function(){
+		assert.throws(function(){
+			ami({}, function(){});
+		}, Error);
+	});
+
+	it('passing invalid mainFunc throws an error', function(){
+		assert.throws(function(){
+			ami([], {});
+		}, Error);
+	});
+});
